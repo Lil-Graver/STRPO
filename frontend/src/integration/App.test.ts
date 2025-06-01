641d8a319082c2c5a3998fe3723ff8596e796f58
Lil-Graver <79765410+Lil-Graver@users.noreply.github.com>
2025-06-02 01:01:29 +0700

    Update App.test.ts

--- frontend/src/integration/App.test.ts
+++ frontend/src/integration/App.test.ts
@@ -1,9 +1,11 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest'
 
 describe('Integration: Backend API', () => {
-  it('should respond with 200 on GET /api/weather', async () => {
-  const res = await fetch('http://localhost:8000/api/weather');
-  expect(res.status).toBe(200);
-  // можно также проверить структуру ответа
-});
-});
+  it('should respond with 200 on GET /api/forecast/', async () => {
+    const res = await fetch('http://localhost:8000/api/forecast/')
+    expect(res.status).toBe(200)
+
+    const json = await res.json()
+    expect(json).toHaveProperty('forecast') // Пример: проверь ключ в ответе
+  })
+})
