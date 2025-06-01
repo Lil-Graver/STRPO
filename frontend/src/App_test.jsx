1e2080f93715f76ebf2e629b552c2428fb630bfd
Lil-Graver <79765410+Lil-Graver@users.noreply.github.com>
2025-06-02 00:52:06 +0700

    Delete frontend/src/App_test.jsx

--- frontend/src/App_test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import App from './App'
-
-test('renders app', () => {
-  render(<App />)
-  expect(screen.getByText(/weather/i)).toBeInTheDocument()
-  
-})
